dbdb18c6e5cfae74b0012a1dbd0ba8f5f5fa83f8
fullstack developer <fullstackdeveloper918@gmail.com>
2024-08-14 23:04:45 +0530

    'changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,27 @@
 
 // export default clerkMiddleware();
 
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+// src/middleware.js
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+export function middleware(req, res, next) {
+  // Custom middleware logic or simply pass through
+  next();
+}
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
